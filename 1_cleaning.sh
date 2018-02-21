d4932f47c12671d8193cbf39a4d3e7e73936ac2c
Jiook Cha <cha.jiook@gmail.com>
2018-02-21 17:13:59 -0500

    Update 1_cleaning.sh

--- 1_cleaning.sh
+++ 1_cleaning.sh
@@ -3,7 +3,7 @@
 cd /ifs/scratch/pimri/posnerlab/1anal/hbn/rawdata
 for s in `cat list_t1`;do 
   dwisource=`find . -wholename "./R*/MRI/*/sub-${s}/dwi*" -type d`
-  dwitarget='../fs/${s}/dwi'
+  dwitarget="../fs/${s}/dwi"
   mkdir ${dwitarget}
   echo $code/fsl_sub_hpc_1 cp ${dwipath}/. ${dwitarget} 
 done
